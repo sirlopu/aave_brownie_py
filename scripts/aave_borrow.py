92035faa91e63ae4baccbd2e836afd4ec0eb3c38
Gabriel S <sirlopug@gmail.com>
2022-03-12 22:44:25 -0800

    erc20 approve, lending pool & borrowable functions

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,6 +1,11 @@
+from code import interact
 from scripts.helpful_scripts import get_account
-from brownie import config, network
+from brownie import config, network, interface
 from scripts.get_weth import get_weth
+from web3 import Web3
+
+# 0.1
+amount = Web3.toWei(0.1, "ether")
 
 
 def main():
@@ -8,3 +13,66 @@ def main():
     erc20_address = config["networks"][network.show_active()]["weth_token"]
     if network.show_active() in ["mainnet-fork"]:
         get_weth()
+    # ABI
+    # Address
+    lending_pool = get_lending_pool()
+    # print(lending_pool)
+    # approve sending out ERC tokens
+    approve_erc20(amount, lending_pool.address, erc20_address, account)
+    print("Depositing...")
+    tx = lending_pool.deposit(
+        erc20_address, amount, account.address, 0, {"from": account}
+    )
+    tx.wait(1)
+    print("Deposited!")
+    borrowable_eth, total_debt_eth = get_borrowable_data(lending_pool, account)
+    print("LETS BORROW IT ALL")
+    # DAI interms of ETH
+    dai_eth_price = get_asset_price(dai_eth_price_feed)
+
+
+def approve_erc20(amount, spender, erc20_address, account):
+    print("Approving ERC20 token...")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved!")
+    return tx
+
+
+def get_lending_pool():
+    lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
+        config["networks"][network.show_active()]["lending_pool_addresses_provider"]
+    )
+    lending_pool_address = lending_pool_addresses_provider.getLendingPool()
+    lending_pool = interface.ILendingPool(lending_pool_address)
+    return lending_pool
+
+
+def get_borrowable_data(lending_pool, account):
+    (
+        total_collateral_eth,
+        total_debt_eth,
+        available_borrow_eth,
+        current_liquidation_threshold,
+        tlv,
+        health_factor,
+    ) = lending_pool.getUserAccountData(account.address)
+    available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
+    total_collateral_eth = Web3.fromWei(total_collateral_eth, "ether")
+    total_debt_eth = Web3.fromWei(total_debt_eth, "ether")
+    print(f"You have {total_collateral_eth} worth of ETH deposited.")
+    print(f"You have {total_debt_eth} worth of ETH borrowed.")
+    print(f"You can borrow {available_borrow_eth} worth of ETH.")
+    return (float(available_borrow_eth), float(total_debt_eth))
+
+
+def get_asset_price():
+    # For mainnet we can just do:
+    # return Contract(f"{pair}.data.eth").latestAnswer() / 1e8
+    dai_eth_price_feed = interface.AggregatorV3Interface(
+        config["networks"][network.show_active()]["dai_eth_price_feed"]
+    )
+    latest_price = Web3.fromWei(dai_eth_price_feed.latestRoundData()[1], "ether")
+    print(f"The DAI/ETH price is {latest_price}")
+    return float(latest_price)
